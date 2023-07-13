a0ab63f295fa3b9ccf9846ce3e3d26be3d5c911f
shahbaz4783 <shahbaz4783@gmail.com>
2023-07-13 16:01:29 +0530

    ui update

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const joinBtn = document.getElementById('join');
 const formBox = document.getElementById('submit-form');
 const closeFormBox = document.getElementById('close');
+const section = document.querySelector('section');
 
 formBox.style.display = 'none';
 
 const showSubmitBox = () => {
     formBox.style.display = 'flex';
+    section.classList.add('blurBackground');
 }
 
 const closeForm = () => {
     formBox.style.display = 'none';
+    section.classList.remove('blurBackground');
 }
 
 joinBtn.addEventListener('click', showSubmitBox)
